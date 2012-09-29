37650d904416dcac3265991c2b52ba7e2cfd0bc6
Sébastien Lerique <seblerique@wanadoo.fr>
2012-09-29 14:48:46 +0200

    Removed GPS location, adapted the FirstLaunchMeasuresActivity accordingly.

--- src/com/brainydroid/daydreaming/FirstLaunchMeasuresActivity.java
+++ src/com/brainydroid/daydreaming/FirstLaunchMeasuresActivity.java
@@ -1,17 +1,13 @@
 package com.brainydroid.daydreaming;
 
 import android.annotation.TargetApi;
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
@@ -21,7 +17,6 @@ import android.widget.TextView;
 public class FirstLaunchMeasuresActivity extends FragmentActivity {
 
 	private TextView textNetworkLocation;
-	private TextView textGPSLocation;
 	private TextView textSettings;
 	private Button buttonSettings;
 	private Button buttonNext;
@@ -32,10 +27,6 @@ public class FirstLaunchMeasuresActivity extends FragmentActivity {
 	private SharedPreferences.Editor eDPrefs;
 	private LocationManager locationManager;
 
-	private boolean networkLocEnabled;
-	private boolean gpsEnabled;
-	private boolean adjustSettingsOff;
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -43,7 +34,6 @@ public class FirstLaunchMeasuresActivity extends FragmentActivity {
 		setContentView(R.layout.activity_first_launch_measures);
 
 		textNetworkLocation = (TextView) findViewById(R.id.firstLaunchMeasures_textNetworkLocation);
-		textGPSLocation = (TextView) findViewById(R.id.firstLaunchMeasures_textGPSLocation);
 		textSettings = (TextView) findViewById(R.id.firstLaunchMeasures_textSettings);
 		buttonSettings = (Button) findViewById(R.id.firstLaunchMeasures_buttonSettings);
 		buttonNext = (Button) findViewById(R.id.firstLaunchMeasures_buttonNext);
@@ -62,9 +52,6 @@ public class FirstLaunchMeasuresActivity extends FragmentActivity {
 	public void onResume() {
 		super.onResume();
 
-		networkLocEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-		gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
 		updateView();
 	}
 
@@ -82,24 +69,21 @@ public class FirstLaunchMeasuresActivity extends FragmentActivity {
 
 	private void updateView() {
 		textNetworkLocation.setCompoundDrawablesWithIntrinsicBounds(
-				networkLocEnabled ? R.drawable.check : R.drawable.cross, 0, 0, 0);
-		textGPSLocation.setCompoundDrawablesWithIntrinsicBounds(
-				gpsEnabled ? R.drawable.check : R.drawable.cross, 0, 0, 0);
+				isNetworkLocEnabled() ? R.drawable.check : R.drawable.cross, 0, 0, 0);
 
 		updateRequestAdjustSettings();
 	}
 
 	private void updateRequestAdjustSettings() {
-
-		if (networkLocEnabled && gpsEnabled) {
+		if (isNetworkLocEnabled()) {
 			setAdjustSettingsOff();
 		} else {
-			if (!networkLocEnabled && !gpsEnabled) {
-				setAdjustSettingsNecessary();
-			} else {
-				setAdjustSettingsOptional();
-			}
+			setAdjustSettingsNecessary();
 		}
+	}
+
+	private boolean isNetworkLocEnabled() {
+		return locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 	}
 
 	@TargetApi(11)
@@ -114,22 +98,6 @@ public class FirstLaunchMeasuresActivity extends FragmentActivity {
 			buttonNext.setVisibility(View.INVISIBLE);
 		}
 		buttonNext.setClickable(false);
-		adjustSettingsOff = false;
-	}
-
-	@TargetApi(11)
-	private void setAdjustSettingsOptional() {
-		textSettings.setText(R.string.firstLaunchMeasures_text_settings_optional);
-		textSettings.setVisibility(View.VISIBLE);
-		buttonSettings.setVisibility(View.VISIBLE);
-		buttonSettings.setClickable(true);
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			buttonNext.setAlpha(1f);
-		} else {
-			buttonNext.setVisibility(View.VISIBLE);
-		}
-		buttonNext.setClickable(true);
-		adjustSettingsOff = false;
 	}
 
 	@TargetApi(11)
@@ -143,7 +111,6 @@ public class FirstLaunchMeasuresActivity extends FragmentActivity {
 			buttonNext.setVisibility(View.VISIBLE);
 		}
 		buttonNext.setClickable(true);
-		adjustSettingsOff = true;
 	}
 
 	public void onClick_buttonSettings(View view) {
@@ -156,16 +123,7 @@ public class FirstLaunchMeasuresActivity extends FragmentActivity {
 	}
 
 	public void onClick_buttonNext(View view) {
-		if (adjustSettingsOff) {
-			launchDashboard();
-		} else {
-			DialogFragment settingsAlert = SettingsAlertDialogFragment.newInstance(
-					R.string.firstLaunchMeasures_alert_title,
-					R.string.firstLaunchMeasures_alert_text,
-					R.string.firstLaunchMeasures_alert_button_ignore,
-					R.string.firstLaunchMeasures_alert_button_gotosettings);
-			settingsAlert.show(getSupportFragmentManager(), "settingsAlert");
-		}
+		launchDashboard();
 	}
 
 	public void launchDashboard() {
@@ -175,55 +133,5 @@ public class FirstLaunchMeasuresActivity extends FragmentActivity {
 		eDPrefs.commit();
 		Intent dashboardIntent = new Intent(this, DashboardActivity.class);
 		startActivity(dashboardIntent);
-	}
-
-	public static class SettingsAlertDialogFragment extends DialogFragment {
-
-		public static SettingsAlertDialogFragment newInstance(int title, int text,
-				int negText, int posText) {
-			SettingsAlertDialogFragment frag = new SettingsAlertDialogFragment();
-			Bundle args = new Bundle();
-			args.putInt("title", title);
-			args.putInt("text", text);
-			args.putInt("negText", negText);
-			args.putInt("posText", posText);
-			frag.setArguments(args);
-			return frag;
-		}
-
-		@TargetApi(11)
-		@Override
-		public Dialog onCreateDialog(Bundle savedInstanceState) {
-			int title = getArguments().getInt("title");
-			int text = getArguments().getInt("text");
-			int negText = getArguments().getInt("negText");
-			int posText = getArguments().getInt("posText");
-
-			AlertDialog.Builder alertSettings = new AlertDialog.Builder(getActivity())
-			.setTitle(title)
-			.setMessage(text)
-			.setNegativeButton(negText,
-					new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int whichButton) {
-					((FirstLaunchMeasuresActivity)getActivity()).launchDashboard();
-				}
-			})
-			.setPositiveButton(posText,
-					new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int whichButton) {
-					((FirstLaunchMeasuresActivity)getActivity()).launchSettings();
-				}
-			});
-
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-				alertSettings.setIconAttribute(android.R.attr.alertDialogIcon);
-			} else {
-				alertSettings.setIcon(android.R.drawable.ic_dialog_alert);
-			}
-
-			return alertSettings.create();
-		}
 	}
 }
