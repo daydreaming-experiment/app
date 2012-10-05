57a138c1c27377f9d8b527ec83ab30da21c9a35b
Sébastien Lerique <seblerique@wanadoo.fr>
2012-10-05 14:47:55 +0200

    Got starting and stopping of service to work.

--- src/com/brainydroid/daydreaming/DashboardActivity.java
+++ src/com/brainydroid/daydreaming/DashboardActivity.java
@@ -1,13 +1,19 @@
 package com.brainydroid.daydreaming;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import com.brainydroid.daydreaming.ExperimentService.LocalBinder;
 
 public class DashboardActivity extends Activity {
 
@@ -16,6 +22,9 @@ public class DashboardActivity extends Activity {
 	private SharedPreferences mFLPrefs;
 	private SharedPreferences mDPrefs;
 	private SharedPreferences.Editor eDPrefs;
+
+	ExperimentService experimentService;
+	boolean mBound = false;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -51,9 +60,37 @@ public class DashboardActivity extends Activity {
 	public void onResume() {
 		super.onResume();
 
-		checkFirstRun();
-		checkServiceUpdateView();
+		//		checkFirstRun();
+		//		checkServiceUpdateView();
 	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		// Unbind from the service
+		if (mBound) {
+			unbindService(mConnection);
+			mBound = false;
+		}
+	}
+
+	private final ServiceConnection mConnection = new ServiceConnection() {
+
+		@Override
+		public void onServiceConnected(ComponentName className,
+				IBinder service) {
+			// We've bound to ExperimentService, cast the IBinder and get ExperimentService instance
+			LocalBinder binder = (LocalBinder)service;
+			experimentService = binder.getService();
+			mBound = true;
+			updateView();
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName arg0) {
+			mBound = false;
+		}
+	};
 
 	private void checkFirstRun() {
 		if (!mFLPrefs.getBoolean(getString(R.pref.firstLaunchCompleted), false)) {
@@ -73,16 +110,23 @@ public class DashboardActivity extends Activity {
 		}
 	}
 
-	private void startQuestionsService() {
-		Toast.makeText(this, "Will start service", Toast.LENGTH_SHORT).show();
-		eDPrefs.putBoolean(getString(R.pref.dashboardExpRunning), true);
-		eDPrefs.commit();
+	private void startExperimentService() {
+		//		Toast.makeText(this, "Will start service", Toast.LENGTH_SHORT).show();
+
+		Intent experimentIntent = new Intent(this, ExperimentService.class);
+		startService(experimentIntent);
+		bindService(experimentIntent, mConnection, Context.BIND_AUTO_CREATE);
 	}
 
-	private void stopQuestionsService() {
-		Toast.makeText(this, "Will stop service", Toast.LENGTH_SHORT).show();
-		eDPrefs.putBoolean(getString(R.pref.dashboardExpRunning), false);
-		eDPrefs.commit();
+	private void stopExperimentService() {
+		//		Toast.makeText(this, "Will stop service", Toast.LENGTH_SHORT).show();
+		if (mBound) {
+			experimentService.stopServiceOnUnbind();
+			unbindService(mConnection);
+			mBound = false;
+		} else {
+			Toast.makeText(this, "Could not stop service: not bound", Toast.LENGTH_SHORT).show();
+		}
 	}
 
 	private boolean isServiceRunning() {
@@ -94,11 +138,18 @@ public class DashboardActivity extends Activity {
 	}
 
 	private void checkService() {
-		if (isServiceRunning() != isServiceShouldRun()) {
+		if (isServiceRunning()) {
+			if (!mBound) {
+				Intent experimentIntent = new Intent(this, ExperimentService.class);
+				bindService(experimentIntent, mConnection, Context.BIND_AUTO_CREATE);
+			}
+
+			if (!isServiceShouldRun()) {
+				stopExperimentService();
+			}
+		} else {
 			if (isServiceShouldRun()) {
-				startQuestionsService();
-			} else {
-				stopQuestionsService();
+				startExperimentService();
 			}
 		}
 	}
--- src/com/brainydroid/daydreaming/ExperimentService.java
+++ src/com/brainydroid/daydreaming/ExperimentService.java
@@ -3,18 +3,34 @@ package com.brainydroid.daydreaming;
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Binder;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class ExperimentService extends Service {
 
 	private SharedPreferences mDPrefs;
 	private SharedPreferences.Editor eDPrefs;
 
+	private boolean stopSelfOnUnbind = false;
+	private final IBinder mBinder = new LocalBinder();
+
+	public class LocalBinder extends Binder {
+		ExperimentService getService() {
+			// Return this instance of ExperimentService so clients can call public methods
+			return ExperimentService.this;
+		}
+	}
+
 	@Override
 	public void onCreate() {
 		mDPrefs = getSharedPreferences(getString(R.pref.dashboardPrefs), MODE_PRIVATE);
 		eDPrefs = mDPrefs.edit();
+
 		eDPrefs.putBoolean(getString(R.pref.dashboardExpRunning), true);
+		eDPrefs.commit();
+
+		Toast.makeText(getApplicationContext(), "Service started", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
@@ -24,13 +40,38 @@ public class ExperimentService extends Service {
 
 	@Override
 	public void onDestroy() {
+		super.onDestroy();
+
 		eDPrefs.putBoolean(getString(R.pref.dashboardExpRunning), false);
+		eDPrefs.commit();
+
+		Toast.makeText(getApplicationContext(), "Service stopped", Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
-		// Don't allow binding
-		return null;
+		Toast.makeText(getApplicationContext(), "Service bound", Toast.LENGTH_SHORT).show();
+		return mBinder;
 	}
 
+	@Override
+	public void onRebind(Intent intent) {
+		Toast.makeText(getApplicationContext(), "Service rebound", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public boolean onUnbind(Intent intent) {
+		super.onUnbind(intent);
+
+		Toast.makeText(getApplicationContext(), "Service unbound", Toast.LENGTH_SHORT).show();
+		if (stopSelfOnUnbind) {
+			stopSelf();
+		}
+
+		return true;
+	}
+
+	public void stopServiceOnUnbind() {
+		stopSelfOnUnbind = true;
+	}
 }
