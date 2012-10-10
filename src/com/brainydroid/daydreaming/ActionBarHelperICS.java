813f78d9ed42bf5933753ef555ece77eb65a8e69
Sébastien Lerique <seblerique@wanadoo.fr>
2012-10-10 22:21:13 +0200

    Small warnings fixed

--- src/com/brainydroid/daydreaming/ActionBarHelperICS.java
+++ src/com/brainydroid/daydreaming/ActionBarHelperICS.java
@@ -18,20 +18,18 @@ package com.brainydroid.daydreaming;
 
 import android.app.Activity;
 import android.content.Context;
-import android.view.Menu;
-import android.view.MenuItem;
 
 /**
  * An extension of {@link com.example.android.actionbarcompat.ActionBarHelper} that provides Android
  * 4.0-specific functionality for IceCreamSandwich devices. It thus requires API level 14.
  */
 public class ActionBarHelperICS extends ActionBarHelperHoneycomb {
-    protected ActionBarHelperICS(Activity activity) {
-        super(activity);
-    }
+	protected ActionBarHelperICS(Activity activity) {
+		super(activity);
+	}
 
-    @Override
-    protected Context getActionBarThemedContext() {
-        return mActivity.getActionBar().getThemedContext();
-    }
+	@Override
+	protected Context getActionBarThemedContext() {
+		return mActivity.getActionBar().getThemedContext();
+	}
 }
