4b0350c70467fca990056d3af9a0d2834f2f12c5
Sébastien Lerique <seblerique@wanadoo.fr>
2013-05-03 23:01:03 +0300

    Document TimeReceiver

--- src/main/java/com/brainydroid/daydreaming/background/TimeReceiver.java
+++ src/main/java/com/brainydroid/daydreaming/background/TimeReceiver.java
@@ -7,9 +7,20 @@ import com.brainydroid.daydreaming.ui.Config;
 import com.google.inject.Inject;
 import roboguice.receiver.RoboBroadcastReceiver;
 
+/**
+ * Start {@link SchedulerService} to reschedule the next {@link
+ * com.brainydroid.daydreaming.db.Poll} when the user's time settings change.
+ * <p/>
+ * This service is only started if the first launch has been completed
+ * (i.e. the user has registered and is participating in the experiment).
+ *
+ * @author Sébastien Lerique
+ * @author Vincent Adam
+ */
 public class TimeReceiver extends RoboBroadcastReceiver {
 
-    public static String TAG = "TimeReceiver";
+    @SuppressWarnings("FieldCanBeLocal")
+    private static String TAG = "TimeReceiver";
 
     @Inject StatusManager statusManager;
 
@@ -23,17 +34,22 @@ public class TimeReceiver extends RoboBroadcastReceiver {
 
         String action = intent.getAction();
 
-        if (action.equals(Intent.ACTION_TIME_CHANGED) || action.equals(Intent.ACTION_TIMEZONE_CHANGED)) {
+        // Were we called because the time settings changed?
+        if (action.equals(Intent.ACTION_TIME_CHANGED) ||
+                action.equals(Intent.ACTION_TIMEZONE_CHANGED)) {
 
             // Info
             Log.i(TAG, "Received ACTION_TIME_CHANGED or ACTION_TIMEZONE_CHANGED");
 
+            // If first launch hasn't been completed, the user doesn't want
+            // anything yet
             if (statusManager.isFirstLaunchCompleted()) {
 
                 // Info
                 Log.i(TAG, "first launch is completed");
                 Log.i(TAG, "starting SchedulerService");
 
+                // Reschedule the next poll
                 Intent schedulerIntent = new Intent(context, SchedulerService.class);
                 context.startService(schedulerIntent);
             }
