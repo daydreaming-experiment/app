5371dceb83763369e7937aede440efed9faecd1f
Sébastien Lerique <seblerique@wanadoo.fr>
2013-05-06 18:19:28 +0300

    Document Answer

--- src/main/java/com/brainydroid/daydreaming/db/Answer.java
+++ src/main/java/com/brainydroid/daydreaming/db/Answer.java
@@ -2,10 +2,29 @@ package com.brainydroid.daydreaming.db;
 
 import android.widget.LinearLayout;
 
+/**
+ * Interface for answers to {@link Poll} {@link Question}s,
+ * sent to server once filled.
+ *
+ * @author Sébastien Lerique
+ * @author Vincent Adam
+ */
 public interface Answer {
 
+    /**
+     * Convert the {@link Answer} to JSON. Implementation depends on the
+     * type of {@link Question} the answer is answering.
+     *
+     * @return JSON representation of the answer
+     */
     public String toJson();
 
+    /**
+     * Parse a layout and fill the instance with the user's answers present
+     * in the layout.
+     *
+     * @param questionLinearLayout The layout to parse through
+     */
     public void getAnswersFromLayout(LinearLayout questionLinearLayout);
 
 }
