2248db3d66497f75a735e601c896e5c5f9395757
Sébastien Lerique <seblerique@wanadoo.fr>
2013-05-21 18:00:28 +0300

    Document AnswerDeserializer, IAnswer and IQuestionDetails. Some code style changes in other files.

--- src/main/java/com/brainydroid/daydreaming/db/IQuestionDetails.java
+++ src/main/java/com/brainydroid/daydreaming/db/IQuestionDetails.java
@@ -1,7 +1,22 @@
 package com.brainydroid.daydreaming.db;
 
+/**
+ * Interface to implement to represent details of a question.
+ * <p/>
+ * See {@link Question} for further details on how this interface plays
+ * along with the other classes and interfaces.
+ *
+ * @author Sébastien Lerique
+ * @author Vincent Adam
+ */
 public interface IQuestionDetails {
 
+    /**
+     * Get the type of question for which this instance is the details field.
+     * <p/>
+     *
+     * @return Question type
+     */
     public String getType();
 
 }
