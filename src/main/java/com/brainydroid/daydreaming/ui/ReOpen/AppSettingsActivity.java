c0fb0bbf33483d6ea7b7c70b9cfa6ea268be3d45
Vincent Adam <vincent.adam87@gmail.com>
2013-09-05 13:15:47 +0100

    Adding time pickers (TODO: load default time or saved times, save times, checktime window consitency)

--- src/main/java/com/brainydroid/daydreaming/ui/ReOpen/AppSettingsActivity.java
+++ src/main/java/com/brainydroid/daydreaming/ui/ReOpen/AppSettingsActivity.java
@@ -1,37 +1,45 @@
 package com.brainydroid.daydreaming.ui.ReOpen;
 
-import android.app.Activity;
+import android.app.*;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import com.brainydroid.daydreaming.R;
 import com.brainydroid.daydreaming.background.Logger;
 import com.brainydroid.daydreaming.background.SchedulerService;
-import com.brainydroid.daydreaming.db.Util;
 import com.brainydroid.daydreaming.ui.FontUtils;
-import com.brainydroid.daydreaming.ui.TimePreference;
-import roboguice.inject.InjectResource;
+import com.brainydroid.daydreaming.ui.TimePickerFragment;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 
-public class AppSettingsActivity extends Activity
-        implements OnSharedPreferenceChangeListener {
+
+@ContentView(R.layout.activity_appsettings_layout)
+public class AppSettingsActivity extends RoboActivity { //implements OnSharedPreferenceChangeListener {
+
+    public static String TIME_FROM = "time_from";
+    public static String TIME_UNTIL = "time_until";
 
     private static String TAG = "AppSettingsActivity";
     private static int MIN_WINDOW_HOURS = 5; // 5 hours (in hours)
 
-    private TimePreference timePreferenceMax;
-    private TimePreference timePreferenceMin;
+   // private TimePreference timePreferenceMax;
+   // private TimePreference timePreferenceMin;
 
     public static final String PREFS_FILE = "prefs";
 
-    SharedPreferences sharedPreferences;
+   // public SharedPreferences sharedPreferences;
 
+    @InjectView(R.id.settings_time_text_from) TextView textview_time_from;
+    @InjectView(R.id.settings_time_text_until) TextView textview_time_until;
 
-    @InjectResource(R.pref.settings_time_window_lb_default) String defaultTimePreferenceMin;
-    @InjectResource(R.pref.settings_time_window_ub_default) String defaultTimePreferenceMax;
+     // @InjectResource(R.pref.settings_time_window_lb_default) String defaultTimePreferenceMin;
+   // @InjectResource(R.pref.settings_time_window_ub_default) String defaultTimePreferenceMax;
+
     //@InjectResource(R.string.settings_time_corrected_1) String timeCorrectedText1;
     //@InjectResource(R.string.settings_time_corrected_2) String timeCorrectedText2;
 
@@ -39,10 +47,18 @@ public class AppSettingsActivity extends Activity
     public void onCreate(Bundle savedInstanceState) {
         Logger.v(TAG, "Creating");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appsettings_layout);
+
+
+
+       // setContentView(R.layout.activity_appsettings_layout);
+         //  textview_time_from = (TextView)findViewById(R.id.settings_time_text_from);
+         //  textview_time_until = (TextView)findViewById(R.id.settings_time_text_until);
+
         ViewGroup godfatherView = (ViewGroup)this.getWindow().getDecorView();
         FontUtils.setRobotoFont(this, godfatherView);
+
         initVars();
+        addListenerOnButton();
     }
 
     @Override
@@ -51,14 +67,14 @@ public class AppSettingsActivity extends Activity
         super.onResume();
 
         // Set up a listener for whenever a key changes
-        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+        //sharedPreferences.registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
     protected void onStop() {
         Logger.v(TAG, "Stopping");
         super.onStop();
-        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+        //sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
     }
 
 
@@ -67,30 +83,85 @@ public class AppSettingsActivity extends Activity
     private void initVars() {
         Logger.d(TAG, "Initializing variables");
 
-        // Load the XML preferences_appsettings file
-        //addPreferencesFromResource(R.layout.preferences_appsettings);
 
-        //PreferenceScreen preferenceScreen = getPreferenceScreen();
-        //sharedPreferences = preferenceScreen.getSharedPreferences();
+        //sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        // Get a reference to the preferences_appsettings
-        //timePreferenceMin = (TimePreference)preferenceScreen.findPreference("time_window_lb_key");
-        //timePreferenceMax = (TimePreference)preferenceScreen.findPreference("time_window_ub_key");
-
-        timePreferenceMin = new TimePreference(getApplicationContext());
+//        timePreferenceMin = new TimePreference(getApplicationContext());
         //timePreferenceMin.setTime(sharedPreferences.getString("time_window_lb_key","00:00"));
-        timePreferenceMax = new TimePreference(getApplicationContext());
+//        timePreferenceMax = new TimePreference(getApplicationContext());
         //timePreferenceMax.setTime(sharedPreferences.getString("time_window_ub_key","00:00"));
 
         //timePreferenceMax = (TimePreference)preferenceScreen.findPreference("time_window_ub_key");
 
 
-        timePreferenceMin.setSummary(timePreferenceMin.getTimeString());
-        timePreferenceMax.setSummary(timePreferenceMax.getTimeString());
+  //      timePreferenceMin.setSummary(timePreferenceMin.getTimeString());
+  //      timePreferenceMax.setSummary(timePreferenceMax.getTimeString());
     }
 
+    public void addListenerOnButton() {
 
+        Logger.v(TAG, "Creating Listeners");
+
+        //TODO: deal with loading defautls when no shared pref and sharedpref otherwise
+
+        String Time_from = "09:00"; //sharedPreferences.getString(TIME_FROM,"00:00");
+        String Time_until = "19:00"; // sharedPreferences.getString(TIME_UNTIL,"00:00");
+
+        //textview_time_from.setText(Time_from);
+        //textview_time_until.setText(Time_until);
+
+        final int hour_from = Integer.parseInt(Time_from.substring(0, 2));
+        final int minute_from = Integer.parseInt(Time_from.substring(3, 5));
+
+        final int hour_until = Integer.parseInt(Time_until.substring(0, 2));
+        final int minute_until = Integer.parseInt(Time_until.substring(3, 5));
+
+
+
+        Logger.v(TAG, "Creating Listeners");
+
+        textview_time_from.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new TimePickerFragment() {
+                    @Override
+                    public Dialog onCreateDialog(Bundle savedInstanceState) {
+                        return new TimePickerDialog(getActivity(), this, hour_from, minute_from, DateFormat.is24HourFormat(getActivity()));
+                    }
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        //TODO Set text and save preference and relaunch scheduler if conflict
+                        textview_time_from.setText(new StringBuilder().append(pad(hourOfDay)).append(":").append(pad(minute)));
+                    }
+                };
+                newFragment.show(getFragmentManager(), "timePicker_from");
+            }
+        });
+
+        textview_time_until.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new TimePickerFragment() {
+                    @Override
+                    public Dialog onCreateDialog(Bundle savedInstanceState) {
+                        return new TimePickerDialog(getActivity(), this, hour_until, minute_until, DateFormat.is24HourFormat(getActivity()));
+                    }
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        //TODO Set text and save preference and relaunch scheduler if conflict
+                        textview_time_until.setText(new StringBuilder().append(pad(hourOfDay)).append(":").append(pad(minute)));
+                    }
+                };
+                newFragment.show(getFragmentManager(), "timePicker_until");
+            }
+        });
+
+
+    }
+
+    /*
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         Logger.d(TAG, "Preferences changed");
@@ -100,26 +171,31 @@ public class AppSettingsActivity extends Activity
                     "it, updating summaries, and starting scheduler " +
                     "service");
             correctTimeWindow();
-            timePreferenceMax.setSummary(timePreferenceMax.getTimeString());
-            timePreferenceMin.setSummary(timePreferenceMin.getTimeString());
+     //       timePreferenceMax.setSummary(timePreferenceMax.getTimeString());
+     //       timePreferenceMin.setSummary(timePreferenceMin.getTimeString());
             startSchedulerService();
         }
     }
+    */
 
+    //TODO: redo the checks
     private void correctTimeWindow() {
         if (!checkTimeWindow()) {
             Logger.d(TAG, "Time window set by user is not allowed, " +
                     "correcting");
-            timePreferenceMin.setTime(defaultTimePreferenceMin);
-            timePreferenceMax.setTime(defaultTimePreferenceMax);
-           // Toast.makeText(this, timeCorrectedText1 + " " + MIN_WINDOW_HOURS +
+  //          timePreferenceMin.setTime(defaultTimePreferenceMin);
+  //          timePreferenceMax.setTime(defaultTimePreferenceMax);
+            // Toast.makeText(this, timeCorrectedText1 + " " + MIN_WINDOW_HOURS +
             //        " " + timeCorrectedText2, Toast.LENGTH_LONG).show();
         } else {
             Logger.w(TAG, "Time window set by user is OK");
         }
     }
 
+    // TODO redo check time without time preference
     private boolean checkTimeWindow() {
+
+        /**
         String timeFirst = timePreferenceMin.getTimeString();
         int firstHour = Util.getHour(timeFirst);
         int firstMinute = Util.getMinute(timeFirst);
@@ -136,6 +212,9 @@ public class AppSettingsActivity extends Activity
         }
 
         return (first + MIN_WINDOW_HOURS * 60) <= last;
+
+        **/
+        return true;
     }
 
     private void startSchedulerService() {
@@ -155,16 +234,38 @@ public class AppSettingsActivity extends Activity
 
     }
 
+    @Override
+    public FragmentManager getFragmentManager() {
+        return super.getFragmentManager();    //To change body of overridden methods use File | Settings | File Templates.
+    }
 
+    /**
     // see http://www.mkyong.com/android/android-time-picker-example/ for timepicker example.. we don't need time preference anymore
     public void onClick_time_from(View v){
-
+        DialogFragment newFragment = new TimePickerFragment(){
+            @Override
+            public void onTimeSet(){
+                //TODO Set text and save preference and relaunch scheduler if conflict
+            }
+        };
+        newFragment.show(getFragmentManager(), "timePicker_from");
     }
 
     public void onClick_time_until(View v){
-
+        DialogFragment newFragment = new TimePickerFragment(){
+            @Override
+            public void onTimeSet(){
+                //TODO Set text and save preference and relaunch scheduler if conflict
+            }
+        };newFragment.show(getFragmentManager(), "timePicker_until");
     }
+     */
 
-
+    private static String pad(int c) {
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
+    }
 
 }
