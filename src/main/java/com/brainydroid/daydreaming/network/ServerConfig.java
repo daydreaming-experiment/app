19a9512bedf7cd6ca224f4afa47e8ed1744165cc
Sébastien Lerique <seblerique@wanadoo.fr>
2013-10-07 01:09:05 +0200

    Update EXP_ID to daydreamingdemo/daydreaming

--- src/main/java/com/brainydroid/daydreaming/network/ServerConfig.java
+++ src/main/java/com/brainydroid/daydreaming/network/ServerConfig.java
@@ -2,7 +2,7 @@ package com.brainydroid.daydreaming.network;
 
 public class ServerConfig {
 
-    public static String EXP_ID = "e276acb578539085518738e4c91310a6cac7c18618b314b295b73f8d27175693";
+    public static String EXP_ID = "a5f0430af850a1c4cc278bf310379a0a65451a19c5a339d099fbb3c65cb14d2b";
     public static String SERVER_NAME = "http://api.qa.naja.cc";
 
     public static String QUESTIONS_VERSION_URL = "http://mehho.net:5001/questionsVersion";
