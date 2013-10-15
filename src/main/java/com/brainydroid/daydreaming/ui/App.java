a75e55aa4f9a6b9ba4ea87c1d6e5f2d9e8249ba1
Sébastien Lerique <sl@mehho.net>
2013-10-15 17:30:52 +0200

    Add crash report code

--- src/main/java/com/brainydroid/daydreaming/ui/App.java
+++ src/main/java/com/brainydroid/daydreaming/ui/App.java
@@ -1,9 +1,22 @@
 package com.brainydroid.daydreaming.ui;
 
 import android.app.Application;
+import com.brainydroid.daydreaming.R;
 import com.brainydroid.daydreaming.background.Logger;
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
 import roboguice.RoboGuice;
 
+@ReportsCrashes(
+        formKey = "",
+        mailTo = "crash-reports@daydreaming-the-app.net",
+        mode = ReportingInteractionMode.DIALOG,
+        resToastText = R.string.crash_toast_text,
+        resDialogText = R.string.crash_dialog_text,
+        resDialogIcon = android.R.drawable.ic_dialog_info,
+        resDialogTitle = R.string.crash_dialog_title,
+        resDialogOkToast = R.string.crash_dialog_ok_toast)
 public class App extends Application {
 
     @SuppressWarnings("FieldCanBeLocal")
@@ -15,6 +28,7 @@ public class App extends Application {
 
         super.onCreate();
 
+        ACRA.init(this);
         RoboGuice.setBaseApplicationInjector(this, RoboGuice.DEFAULT_STAGE,
                 RoboGuice.newDefaultRoboModule(this), new AppModule());
     }
