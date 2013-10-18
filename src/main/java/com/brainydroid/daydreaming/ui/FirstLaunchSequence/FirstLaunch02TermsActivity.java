b1891204ff0ab8334653365a1f5b186f56a8463c
Vincent Adam <vincent.adam87@gmail.com>
2013-10-18 16:33:41 +0100

    externalizing links in additional text in terms activity

--- src/main/java/com/brainydroid/daydreaming/ui/FirstLaunchSequence/FirstLaunch02TermsActivity.java
+++ src/main/java/com/brainydroid/daydreaming/ui/FirstLaunchSequence/FirstLaunch02TermsActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -11,9 +12,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.brainydroid.daydreaming.R;
 import com.brainydroid.daydreaming.background.Logger;
+import com.brainydroid.daydreaming.db.Util;
 import com.brainydroid.daydreaming.ui.AlphaButton;
 import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Activity at first launch
@@ -36,8 +41,9 @@ public class FirstLaunch02TermsActivity extends FirstLaunchActivity implements S
     @InjectView(R.id.firstLaunchTerms_moreConsent_button)
     protected TextView more_consent_button;
     @InjectView(R.id.firstLaunchTerms_moreConsent_text)
+    protected TextView more_consent_links;
+    @InjectView(R.id.firstLaunchTerms_moreConsent_links)
     protected TextView more_consent_text;
-
     @InjectView(R.id.firstLaunchTerms_Scrollview) ScrollViewExt sv;
     @InjectView(R.id.firstLaunchTerms_buttonAgree)
     protected AlphaButton agreeButton;
@@ -88,32 +94,32 @@ public class FirstLaunch02TermsActivity extends FirstLaunchActivity implements S
 
         if (readtheterms == false){
 
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                this);
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                    this);
 
-        // set title
-        alertDialogBuilder.setTitle("Agreement");
+            // set title
+            alertDialogBuilder.setTitle("Agreement");
 
-        // set dialog message
-        alertDialogBuilder
-                .setMessage("You haven't read the terms. Are you sure you want to carry on?")
-                .setCancelable(false)
-                .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,int id) {
-                        launchNextActivity(FirstLaunch03ProfileActivity.class);
-                    }
-                })
-                .setNegativeButton("No",new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,int id) {
-                         dialog.cancel();
-                    }
-                });
+            // set dialog message
+            alertDialogBuilder
+                    .setMessage("You haven't read the terms. Are you sure you want to carry on?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,int id) {
+                            launchNextActivity(FirstLaunch03ProfileActivity.class);
+                        }
+                    })
+                    .setNegativeButton("No",new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,int id) {
+                            dialog.cancel();
+                        }
+                    });
 
-        // create alert dialog
-        AlertDialog alertDialog = alertDialogBuilder.create();
+            // create alert dialog
+            AlertDialog alertDialog = alertDialogBuilder.create();
 
-        // show it
-        alertDialog.show();        }
+            // show it
+            alertDialog.show();        }
         else {
             launchNextActivity(FirstLaunch03ProfileActivity.class);
         }
@@ -155,6 +161,17 @@ public class FirstLaunch02TermsActivity extends FirstLaunchActivity implements S
             public void onClick(View view) {
                 more_consent_text.setText(Html.fromHtml(getString(R.string.more_terms_html)));
                 Linkify.addLinks(more_consent_text, Linkify.WEB_URLS | Linkify.EMAIL_ADDRESSES);
+
+                try {
+                    InputStream termsInputStream = getResources().openRawResource(R.raw.links);
+                    more_consent_links.setText(Util.convertStreamToString(termsInputStream));
+                    termsInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+                more_consent_links.setMovementMethod(LinkMovementMethod.getInstance());
+                Linkify.addLinks(more_consent_links,Linkify.WEB_URLS | Linkify.EMAIL_ADDRESSES);
+
             }
         });
 
