3a7ba2bbeb1bf8743030e8412c8a7d84fd247712
Vincent Adam <vincent.adam87@gmail.com>
2013-10-27 16:10:01 +0000

    adding launcher icon and status bar notification icon

--- src/main/java/com/brainydroid/daydreaming/background/PollService.java
+++ src/main/java/com/brainydroid/daydreaming/background/PollService.java
@@ -129,7 +129,7 @@ public class PollService extends RoboService {
         .setContentTitle(getString(R.string.pollNotification_title))
         .setContentText(getString(R.string.pollNotification_text))
         .setContentIntent(contentIntent)
-        .setSmallIcon(android.R.drawable.ic_dialog_info)
+        .setSmallIcon(R.drawable.ic_stat_notify_small_daydreaming)
         .setAutoCancel(true)
         .setOnlyAlertOnce(true)
         .setDefaults(flags)
