e0d849319b99ddade07062495f266b9f94f18aa0
Sébastien Lerique <sl@mehho.net>
2014-07-21 20:14:48 +0200

    Fix bug no.1: CryptoStorage not changing mode
    The mode for CryptoStorage was set in the constructor, only used once
    per app instance (CryptoStorage is a singleton). So whenever the mode
    changed, CryptoStorage wouldn't update. Now changed to using dynamic
    files that update with the mode.

--- daydreaming/src/main/java/com/brainydroid/daydreaming/network/CryptoStorage.java
+++ daydreaming/src/main/java/com/brainydroid/daydreaming/network/CryptoStorage.java
@@ -15,6 +15,7 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Singleton
 public class CryptoStorage {
@@ -33,37 +34,68 @@ public class CryptoStorage {
     @Inject ServerTalker serverTalker;
     @Inject Json json;
     @Inject Context context;
+    @Inject StatusManager statusManager;
 
-    private final File maiIdFile;
-    private final File publicFile;
-    private final File privateFile;
+    private final File storageDir;
+    @Inject private HashMap<String,File> maiIdFiles;
+    @Inject private HashMap<String,File> publicFiles;
+    @Inject private HashMap<String,File> privateFiles;
 
     @Inject
-    public CryptoStorage(Application application, StatusManager statusManager) {
-        Logger.d(TAG, "Initializing CryptoStorage for profile {}", statusManager.getCurrentModeName());
+    public CryptoStorage(Application application) {
+        Logger.d(TAG, "Initializing CryptoStorage");
+        storageDir = application.getDir(STORAGE_DIRNAME, Context.MODE_PRIVATE);
+    }
 
-        File storageDir = application.getDir(statusManager.getCurrentModeName() + STORAGE_DIRNAME,
-                Context.MODE_PRIVATE);
-        maiIdFile = new File(storageDir, MAI_ID_FILENAME);
-        publicFile = new File(storageDir, PUBLIC_FILENAME);
-        privateFile = new File(storageDir, PRIVATE_FILENAME);
+    private synchronized File getMaiIdFile() {
+        String currentModeName = statusManager.getCurrentModeName();
+        Logger.v(TAG, "{} - Getting maiIdFile", currentModeName);
+
+        if (! maiIdFiles.containsKey(currentModeName)) {
+            maiIdFiles.put(currentModeName,
+                    new File(storageDir, currentModeName + MAI_ID_FILENAME));
+        }
+        return maiIdFiles.get(currentModeName);
+    }
+
+    private synchronized File getPublicFile() {
+        String currentModeName = statusManager.getCurrentModeName();
+        Logger.v(TAG, "{} - Getting publicFile", currentModeName);
+
+        if (! publicFiles.containsKey(currentModeName)) {
+            publicFiles.put(currentModeName,
+                    new File(storageDir, currentModeName + PUBLIC_FILENAME));
+        }
+        return publicFiles.get(currentModeName);
+    }
+
+    private synchronized File getPrivateFile() {
+        String currentModeName = statusManager.getCurrentModeName();
+        Logger.v(TAG, "{} - Getting privateFile", currentModeName);
+
+        if (! privateFiles.containsKey(currentModeName)) {
+            privateFiles.put(currentModeName,
+                    new File(storageDir, currentModeName + PRIVATE_FILENAME));
+        }
+        return privateFiles.get(currentModeName);
     }
 
     public synchronized void onReady(CryptoStorageCallback callback) {
         if (!hasStoredKeyPairAndMaiId()) {
-            Logger.i(TAG, "CryptoStorage not ready -> " +
-                    "registering on the server");
+            Logger.i(TAG, "{} - CryptoStorage not ready -> " +
+                    "registering on the server", statusManager.getCurrentModeName());
             register(callback);
         } else {
-            Logger.i(TAG, "CryptoStorage is ready -> calling back callback " +
-                    "straight away");
+            Logger.i(TAG, "{} - CryptoStorage is ready -> calling back callback " +
+                    "straight away", statusManager.getCurrentModeName());
             callback.onCryptoStorageReady(true);
         }
     }
 
     public synchronized void register(CryptoStorageCallback callback) {
 
-        Logger.d(TAG, "Generating a keypair for registration");
+        Logger.d(TAG, "{} - Generating a keypair for registration",
+                statusManager.getCurrentModeName());
         final KeyPair kp = crypto.generateKeyPairNamedCurve(DEFAULT_CURVE_NAME);
         final CryptoStorageCallback parentCallback = callback;
 
@@ -74,13 +106,14 @@ public class CryptoStorage {
 
             @Override
             public void onHttpConversationFinished(boolean success, String serverAnswer) {
-                Logger.d(TAG, "Registration HttpConversation finished");
+                Logger.d(TAG, "{} - Registration HttpConversation finished",
+                        statusManager.getCurrentModeName());
 
                 boolean storageSuccess = false;
 
                 if (success) {
-                    Logger.d(TAG, "Registration HttpConversation " +
-                            "successful");
+                    Logger.d(TAG, "{} - Registration HttpConversation " +
+                            "successful", statusManager.getCurrentModeName());
                     ProfileWrapper registrationAnswer = json.fromJson(serverAnswer,
                             ProfileWrapper.class);
                     // TODO: handle the case where returned JSON is in fact an error.
@@ -91,10 +124,11 @@ public class CryptoStorage {
                     storageSuccess = storeKeyPairAndMaiId(kp, maiId);
 
                     if (storageSuccess) {
-                        Logger.i(TAG, "Full registration successful");
+                        Logger.i(TAG, "{} - Full registration successful",
+                                statusManager.getCurrentModeName());
                     } else {
-                        Logger.e(TAG, "Registration successful on server " +
-                                "but failed to store locally");
+                        Logger.e(TAG, "{} - Registration successful on server " +
+                                "but failed to store locally", statusManager.getCurrentModeName());
                     }
                 }
 
@@ -103,28 +137,28 @@ public class CryptoStorage {
 
         };
 
-        Logger.i(TAG, "Launching registration");
+        Logger.i(TAG, "{} - Launching registration", statusManager.getCurrentModeName());
         serverTalker.register(kp, registrationCallback);
     }
 
     private synchronized boolean hasStoredMaiId() {
-        return maiIdFile.exists();
+        return getMaiIdFile().exists();
     }
 
     private synchronized boolean hasStoredPrivateKey() {
-        return privateFile.exists();
+        return getPrivateFile().exists();
     }
 
     private synchronized boolean hasStoredPublicKey() {
-        return publicFile.exists();
+        return getPublicFile().exists();
     }
 
     public synchronized boolean hasStoredKeyPairAndMaiId() {
         if (hasStoredPrivateKey() && hasStoredPublicKey() && hasStoredMaiId()) {
             return true;
         } else {
-            Logger.w(TAG, "Incomplete store: missing a key or the maiId -> " +
-                    "clearing the whole store");
+            Logger.w(TAG, "{} - Incomplete store: missing a key or the maiId -> " +
+                    "clearing the whole store", statusManager.getCurrentModeName());
             clearStore();
             return false;
         }
@@ -134,99 +168,110 @@ public class CryptoStorage {
         try {
             clearStore();
 
-            if (maiIdFile.createNewFile()) {
-                Logger.d(TAG, "Created new file for maiId");
+            if (getMaiIdFile().createNewFile()) {
+                Logger.d(TAG, "{} - Created new file for maiId",
+                        statusManager.getCurrentModeName());
             } else {
-                Logger.w(TAG, "Overwriting existing file for maiId");
+                Logger.w(TAG, "{} - Overwriting existing file for maiId",
+                        statusManager.getCurrentModeName());
             }
-            BufferedWriter maiIdBuf = new BufferedWriter(new FileWriter(maiIdFile));
+            BufferedWriter maiIdBuf = new BufferedWriter(new FileWriter(getMaiIdFile()));
             maiIdBuf.write(maiId);
             maiIdBuf.close();
-            Logger.d(TAG, "Written maiId to file");
+            Logger.d(TAG, "{} - Written maiId to file", statusManager.getCurrentModeName());
 
-            if (publicFile.createNewFile()) {
-                Logger.d(TAG, "Created new file for public key");
+            if (getPublicFile().createNewFile()) {
+                Logger.d(TAG, "{} - Created new file for public key",
+                        statusManager.getCurrentModeName());
             } else {
-                Logger.w(TAG, "Overwriting existing file for public key");
+                Logger.w(TAG, "{} - Overwriting existing file for public key",
+                        statusManager.getCurrentModeName());
             }
-            BufferedWriter publicBuf = new BufferedWriter(new FileWriter(publicFile));
+            BufferedWriter publicBuf = new BufferedWriter(new FileWriter(getPublicFile()));
             publicBuf.write(Crypto.base64Encode(kp.getPublic().getEncoded()));
             publicBuf.close();
-            Logger.d(TAG, "Written public key to file");
+            Logger.d(TAG, "{} - Written public key to file", statusManager.getCurrentModeName());
 
-            if (privateFile.createNewFile()) {
-                Logger.d(TAG, "Created new file for private key");
+            if (getPrivateFile().createNewFile()) {
+                Logger.d(TAG, "{} - Created new file for private key",
+                        statusManager.getCurrentModeName());
             } else {
-                Logger.w(TAG, "Overwriting existing file for private key");
+                Logger.w(TAG, "{} - Overwriting existing file for private key",
+                        statusManager.getCurrentModeName());
             }
-            BufferedWriter privateBuf = new BufferedWriter(new FileWriter(privateFile));
+            BufferedWriter privateBuf = new BufferedWriter(new FileWriter(getPrivateFile()));
             privateBuf.write(Crypto.base64Encode(kp.getPrivate().getEncoded()));
             privateBuf.close();
-            Logger.d(TAG, "Written private key to file");
+            Logger.d(TAG, "{} - Written private key to file", statusManager.getCurrentModeName());
 
             return true;
         } catch (IOException e) {
-            Logger.e(TAG, "IO error creating files for crypto storage");
+            Logger.e(TAG, "{} - IO error creating files for crypto storage",
+                    statusManager.getCurrentModeName());
             throw new RuntimeException(e);
         }
     }
 
     public synchronized String getMaiId() {
-        Logger.d(TAG, "Reading maiId from file");
+        Logger.d(TAG, "{} - Reading maiId from file", statusManager.getCurrentModeName());
 
         try {
             BufferedReader buf;
-            buf = new BufferedReader(new FileReader(maiIdFile));
+            buf = new BufferedReader(new FileReader(getMaiIdFile()));
             String maiId = buf.readLine();
             buf.close();
             return maiId;
         } catch (FileNotFoundException e) {
-            Logger.e(TAG, "maiId file not found");
+            Logger.e(TAG, "{} - maiId file not found", statusManager.getCurrentModeName());
             throw new RuntimeException(e);
         } catch (IOException e) {
-            Logger.e(TAG, "IO error reading maiId file");
+            Logger.e(TAG, "{} - IO error reading maiId file", statusManager.getCurrentModeName());
             throw new RuntimeException(e);
         }
     }
 
     public synchronized PublicKey getPublicKey() {
-        Logger.d(TAG, "Reading public key from file");
+        Logger.d(TAG, "{} - Reading public key from file", statusManager.getCurrentModeName());
 
         try {
             BufferedReader buf;
-            buf = new BufferedReader(new FileReader(publicFile));
+            buf = new BufferedReader(new FileReader(getPublicFile()));
             String keyStr = buf.readLine();
             buf.close();
             return crypto.readPublicKey(keyStr);
         } catch (FileNotFoundException e) {
-            Logger.e(TAG, "Public key file not found");
+            Logger.e(TAG, "{} - Public key file not found", statusManager.getCurrentModeName());
             throw new RuntimeException(e);
         } catch (IOException e) {
-            Logger.e(TAG, "IO error reading public key file");
+            Logger.e(TAG, "{} - IO error reading public key file",
+                    statusManager.getCurrentModeName());
             throw new RuntimeException(e);
         } catch (InvalidKeySpecException e) {
-            Logger.e(TAG, "Public key read from file but badly formatted");
+            Logger.e(TAG, "{} - Public key read from file but badly formatted",
+                    statusManager.getCurrentModeName());
             throw new RuntimeException(e);
         }
     }
 
     public synchronized PrivateKey getPrivateKey() {
-        Logger.d(TAG, "Reading private key from file");
+        Logger.d(TAG, "{} - Reading private key from file", statusManager.getCurrentModeName());
 
         try {
             BufferedReader buf;
-            buf = new BufferedReader(new FileReader(privateFile));
+            buf = new BufferedReader(new FileReader(getPrivateFile()));
             String keyStr = buf.readLine();
             buf.close();
             return crypto.readPrivateKey(keyStr);
         } catch (FileNotFoundException e) {
-            Logger.e(TAG, "Private key file not found");
+            Logger.e(TAG, "{} - Private key file not found", statusManager.getCurrentModeName());
             throw new RuntimeException(e);
         } catch (IOException e) {
-            Logger.e(TAG, "IO error reading private key file");
+            Logger.e(TAG, "{} - IO error reading private key file",
+                    statusManager.getCurrentModeName());
             throw new RuntimeException(e);
         } catch (InvalidKeySpecException e) {
-            Logger.e(TAG, "Private key read from file but badly formatted");
+            Logger.e(TAG, "{} - Private key read from file but badly formatted",
+                    statusManager.getCurrentModeName());
             throw new RuntimeException(e);
         }
     }
@@ -237,22 +282,22 @@ public class CryptoStorage {
     }
 
     private synchronized boolean clearMaiId() {
-        Logger.v(TAG, "Clearing maiId file");
-        return maiIdFile.delete();
+        Logger.v(TAG, "{} - Clearing maiId file", statusManager.getCurrentModeName());
+        return getMaiIdFile().delete();
     }
 
     private synchronized boolean clearPrivateKey() {
-        Logger.v(TAG, "Clearing private key file");
-        return privateFile.delete();
+        Logger.v(TAG, "{} - Clearing private key file", statusManager.getCurrentModeName());
+        return getPrivateFile().delete();
     }
 
     private synchronized boolean clearPublicKey() {
-        Logger.v(TAG, "Clearing public key file");
-        return publicFile.delete();
+        Logger.v(TAG, "{} - Clearing public key file", statusManager.getCurrentModeName());
+        return getPublicFile().delete();
     }
 
     public synchronized boolean clearStore() {
-        Logger.v(TAG, "Clearing whole store");
+        Logger.v(TAG, "{} - Clearing whole store", statusManager.getCurrentModeName());
         return clearPrivateKey() && clearPublicKey() && clearMaiId();
     }
 
@@ -262,10 +307,11 @@ public class CryptoStorage {
 
     private synchronized byte[] sign(byte[] data, PrivateKey privateKey) {
         try {
-            Logger.d(TAG, "Signing data");
+            Logger.d(TAG, "{} - Signing data", statusManager.getCurrentModeName());
             return crypto.sign(privateKey, data);
         } catch (InvalidKeyException e) {
-            Logger.e(TAG, "Asked to sign data but our key was invalid");
+            Logger.e(TAG, "{} - Asked to sign data but our key was invalid",
+                    statusManager.getCurrentModeName());
             throw new RuntimeException(e);
         }
     }
@@ -275,7 +321,7 @@ public class CryptoStorage {
     }
 
     public synchronized String signJws(String data, PrivateKey privateKey) {
-        Logger.i(TAG, "Creating JWS for data");
+        Logger.i(TAG, "{} - Creating JWS for data", statusManager.getCurrentModeName());
 
         String b64Header = Crypto.base64urlEncode(JWS_HEADER.getBytes());
         String b64Payload = Crypto.base64urlEncode(data.getBytes());
