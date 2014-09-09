2d819a1aa4fd17b741c626105386f411ce255dcb
Sébastien Lerique <sl@mehho.net>
2014-09-09 21:48:31 +0200

    Update todo

--- daydreaming/src/main/java/com/brainydroid/daydreaming/ui/dashboard/DashboardActivity.java
+++ daydreaming/src/main/java/com/brainydroid/daydreaming/ui/dashboard/DashboardActivity.java
@@ -255,7 +255,7 @@ public class DashboardActivity extends RoboFragmentActivity {
     }
 
     // TODO: disable "see results" button before the results are available
-    // TODO: something should happen once the results are available
+    // TODO: user should be notified once the results are available
     private void updateRunningTimeFromTimestamp(long timestampNow) {
         Logger.d(TAG, "Updating running time with timestamp {}", timestampNow);
         long expStartTimestamp = statusManager.getExperimentStartTimestamp();
