8dcadd627097f376769b03243457fc303a6032a5
Sébastien Lerique <sl@mehho.net>
2014-09-14 14:10:17 +0200

    Fix #218

--- daydreaming/src/main/java/com/brainydroid/daydreaming/ui/dashboard/DashboardActivity.java
+++ daydreaming/src/main/java/com/brainydroid/daydreaming/ui/dashboard/DashboardActivity.java
@@ -281,6 +281,11 @@ public class DashboardActivity extends RoboFragmentActivity {
 
     // TODO: user should be notified once the results are available
     private void updateRunningTimeFromTimestamp(long timestampNow) {
+        if (statusManager.areParametersUpdated()) {
+            Logger.v(TAG, "Parameters not updated, not setting running time views");
+            return;
+        }
+        
         Logger.d(TAG, "Updating running time with timestamp {}", timestampNow);
         long expStartTimestamp = statusManager.getExperimentStartTimestamp();
 
