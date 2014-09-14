ba933c65ad1f23127098483b2d8150df9f6b31dd
Sébastien Lerique <sl@mehho.net>
2014-09-14 21:22:11 +0200

    Implement ManySlidersAnswer

--- daydreaming/src/main/java/com/brainydroid/daydreaming/sequence/ManySlidersAnswer.java
+++ daydreaming/src/main/java/com/brainydroid/daydreaming/sequence/ManySlidersAnswer.java
@@ -1,7 +1,22 @@
 package com.brainydroid.daydreaming.sequence;
 
-/**
- * Created by sl on 14/09/14.
- */
-public class ManySlidersAnswer {
+import com.brainydroid.daydreaming.background.Logger;
+import com.brainydroid.daydreaming.db.Views;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.google.inject.Inject;
+
+import java.util.HashMap;
+
+public class ManySlidersAnswer implements IAnswer {
+
+    @SuppressWarnings("FieldCanBeLocal")
+    private static String TAG = "ManySlidersAnswer";
+
+    @JsonView(Views.Public.class)
+    @Inject HashMap<String, Integer> sliders;
+
+    public synchronized void addSlider(String sliderText, int sliderValue) {
+        Logger.v(TAG, "Adding slider {0} with value {1}", sliderText, sliderValue);
+        sliders.put(sliderText, sliderValue);
+    }
 }
