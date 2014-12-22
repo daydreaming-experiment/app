6d41458b04d64498d0062fc62d218a197b2fbc31
Sébastien Lerique <sl@mehho.net>
2014-12-22 18:41:18 -0300

    Streamline botherWindow storage format

--- daydreaming/src/main/java/com/brainydroid/daydreaming/ui/dashboard/SettingsActivity.java
+++ daydreaming/src/main/java/com/brainydroid/daydreaming/ui/dashboard/SettingsActivity.java
@@ -22,10 +22,8 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.brainydroid.daydreaming.R;
-import com.brainydroid.daydreaming.background.ErrorHandler;
 import com.brainydroid.daydreaming.background.Logger;
 import com.brainydroid.daydreaming.background.StatusManager;
-import com.brainydroid.daydreaming.db.Json;
 import com.brainydroid.daydreaming.db.ProfileStorage;
 import com.brainydroid.daydreaming.db.Util;
 import com.brainydroid.daydreaming.ui.FontUtils;
@@ -126,45 +124,20 @@ public class SettingsActivity extends RoboFragmentActivity {
         Calendar cal = Calendar.getInstance();
         cal.add(Calendar.DATE, 1);
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String formatted = format.format(cal.getTime());
-        return formatted;
+        return format.format(cal.getTime());
     }
 
-
-//    public class BotherWindowMapWrapper {
-//        private HashMap<String, String> botherWindowMap;
-//
-//        public BotherWindowMapWrapper() { }
-//
-//        public void setBotherWindowMap(HashMap<String, String> botherWindowMap_) {
-//            this.botherWindowMap = botherWindowMap_;
-//        }
-//        public HashMap<String, String> getBotherWindowMap() {
-//            return this.botherWindowMap;
-//        }
-//
-//    }
-
-
-
-
     public void updateBotherWindowMap() {
-        // get hashmap from shared preferences through profile
+        // Get HashMap from shared preferences through profile
         HashMap<String, String> botherWindowMap = profileStorage.getBotherWindowMap();
-        // update hashmap
-        String timeFrom = sharedPreferences.getString(TIME_FROM, defaultTimePreferenceMin);
-        String[] fromParts = timeFrom.split(":");
-        final int minuteFrom = Integer.parseInt(fromParts[1]);
-        final int hourFrom = Integer.parseInt(fromParts[0]);
-        String timeUntil = sharedPreferences.getString(TIME_UNTIL, defaultTimePreferenceMax);
-        String[] untilParts = timeUntil.split(":");
-        final int minuteUntil = Integer.parseInt(untilParts[1]);
-        final int hourUntil = Integer.parseInt(untilParts[0]);
 
-        String botherwindow = "{'from':" + hourFrom + ":" + pad(minuteFrom) +
-                ", 'until':" + hourUntil + ":" + pad(minuteUntil) + "}";
-        botherWindowMap.put(getNowString(), botherwindow);
-        // save hashmap into shared preferences and profile
+        // Update HashMap
+        String timeFrom = sharedPreferences.getString(TIME_FROM, defaultTimePreferenceMin);
+        String timeUntil = sharedPreferences.getString(TIME_UNTIL, defaultTimePreferenceMax);
+        String botherWindow = timeFrom + "|" + timeUntil;
+        botherWindowMap.put(getNowString(), botherWindow);
+
+        // Save HashMap into shared preferences and profile
         profileStorage.setBotherWindowMap(botherWindowMap);
     }
 
